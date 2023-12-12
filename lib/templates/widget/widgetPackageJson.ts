201754edd6db5f1e600bbe45e47a69c19b3cb609
Alexandr Petyaykin <saneksa2@gmail.com>
2023-12-12 09:31:42 +0300

    fix: обновлены версии библиотек в шаблоне виджета

--- lib/templates/widget/widgetPackageJson.ts
+++ lib/templates/widget/widgetPackageJson.ts
@@ -17,25 +17,25 @@ export const WIDGET_PACKAGE_JSON_TEMPLATE = `\
     "react-dom": "18.2.0"
   },
   "devDependencies": {
-    "@babel/core": "7.23.3",
-    "@babel/preset-env": "7.23.3",
+    "@babel/core": "7.23.6",
+    "@babel/preset-env": "7.23.6",
     "@babel/preset-react": "7.23.3",
     "@babel/preset-typescript": "7.23.3",
     "@infomaximum/package-cli": "^{{packageCliVersion}}",
-    "@types/jest": "29.5.10",
-    "@types/react": "18.2.39",
+    "@types/jest": "29.5.11",
+    "@types/react": "18.2.43",
     "@types/react-dom": "18.2.17",
-    "@typescript-eslint/eslint-plugin": "6.13.0",
-    "@typescript-eslint/parser": "6.13.0",
-    "eslint": "8.54.0",
+    "@typescript-eslint/eslint-plugin": "6.14.0",
+    "@typescript-eslint/parser": "6.14.0",
+    "eslint": "8.55.0",
     "eslint-plugin-react": "7.33.2",
     "eslint-plugin-react-hooks": "4.6.0",
     "jest": "29.7.0",
     "jest-canvas-mock": "2.5.2",
     "jest-environment-jsdom": "29.7.0",
     "jest-environment-jsdom-global": "4.0.0",
-    "prettier": "3.1.0",
-    "typescript": "~5.3.2"
+    "prettier": "3.1.1",
+    "typescript": "~5.3.3"
   },
   "browserslist": [
     "defaults and supports es6-module"
